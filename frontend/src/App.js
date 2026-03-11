c0ce7e09360eb1d63abe8fe738179e60deb26315
Varad Mandhare <varadmandhare924@gmail.com>
2026-03-11 22:39:29 +0530

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,13 +1,1 @@
-import React from 'react';
-import Game from './pages/Game';
-import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <Game />
-    </div>
-  );
-}
-
-export default App;
